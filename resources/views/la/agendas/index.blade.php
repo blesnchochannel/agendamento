7e4544cbc3d5a521bee2b9491019c9013e8ea625
RAFAEL SOUZA BORGES <blesnchochannel@outlook.com>
2018-02-10 00:37:33 -0200

    Agendas
    Iniciando trabalhos com as agendas e calendários

--- resources/views/la/agendas/index.blade.php
+++ resources/views/la/agendas/index.blade.php
@@ -8,42 +8,61 @@
 
 @section("headerElems")
 @la_access("Agendas", "create")
-	<button class="btn btn-success btn-sm pull-right" data-toggle="modal" data-target="#AddModal">Adicionar  Agenda</button>
+<button class="btn btn-success btn-sm pull-right" data-toggle="modal" data-target="#AddModal">Adicionar Agenda</button>
 @endla_access
 @endsection
 
 @section("main-content")
 
-@if (count($errors) > 0)
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-
-<div class="box box-success">
-	<!--<div class="box-header"></div>-->
-	<div class="box-body">
-		<table id="example1" class="table table-bordered">
-		<thead>
-		<tr class="success">
-			@foreach( $listing_cols as $col )
-			<th>{{ $module->fields[$col]['label'] or ucfirst($col) }}</th>
-			@endforeach
-			@if($show_actions)
-			<th>Ações</th>
-			@endif
-		</tr>
-		</thead>
-		<tbody>
-			
-		</tbody>
-		</table>
+<section class="content">
+	<!-- Small boxes (Stat box) -->
+	<!-- Main row -->
+	<div class="row">
+		<section class="col-lg-12 connectedSortable">
+			<div class="agenda-calendario">
+				<div id="calendarContainer"></div>
+				<div id="organizerContainer"></div>
+			</div>
+		</section>
 	</div>
-</div>
+	<div class="row">
+		<hr>
+	</div>
+	<div class="row">
+		<section class="col-lg-12 connectedSortable">
+			@if (count($errors) > 0)
+			<div class="alert alert-danger">
+				<ul>
+					@foreach ($errors->all() as $error)
+					<li>{{ $error }}</li>
+					@endforeach
+				</ul>
+			</div>
+			@endif
+
+			<div class="box box-success">
+				<!--<div class="box-header"></div>-->
+				<div class="box-body">
+					<table id="example1" class="table table-bordered">
+						<thead>
+							<tr class="success">
+								@foreach( $listing_cols as $col )
+								<th>{{ $module->fields[$col]['label'] or ucfirst($col) }}</th>
+								@endforeach
+								@if($show_actions)
+								<th>Ações</th>
+								@endif
+							</tr>
+						</thead>
+						<tbody>
+
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</section>
+	</div>
+</section>
 
 @la_access("Agendas", "create")
 <div class="modal fade" id="AddModal" role="dialog" aria-labelledby="myModalLabel">
@@ -56,51 +75,149 @@
 			{!! Form::open(['action' => 'LA\AgendasController@store', 'id' => 'agenda-add-form']) !!}
 			<div class="modal-body">
 				<div class="box-body">
-                    @la_form($module)
-					
+					@la_form($module)
+
 					{{--
-					@la_input($module, 'nome')
-					@la_input($module, 'aplicador')
-					@la_input($module, 'agendamentos')
-					--}}
+						@la_input($module, 'nome')
+						@la_input($module, 'aplicador')
+						@la_input($module, 'agendamentos')
+						--}}
+					</div>
 				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
+					{!! Form::submit( 'Enviar', ['class'=>'btn btn-success']) !!}
+				</div>
+				{!! Form::close() !!}
 			</div>
-			<div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">Fechar</button>
-				{!! Form::submit( 'Enviar', ['class'=>'btn btn-success']) !!}
-			</div>
-			{!! Form::close() !!}
 		</div>
 	</div>
-</div>
-@endla_access
+	@endla_access
 
-@endsection
+	@endsection
 
-@push('styles')
-<link rel="stylesheet" type="text/css" href="{{ asset('la-assets/plugins/datatables/datatables.min.css') }}"/>
-@endpush
+	@push('styles')
+	<link rel="stylesheet" type="text/css" href="{{ asset('la-assets/plugins/datatables/datatables.min.css') }}"/>
+	<link rel="stylesheet" type="text/css" href="{{ asset('la-assets/css/pages/agendas.css') }}"/>
+	@endpush
 
-@push('scripts')
-<script src="{{ asset('la-assets/plugins/datatables/datatables.min.js') }}"></script>
-<script>
-$(function () {
-	$("#example1").DataTable({
-		processing: true,
-        serverSide: true,
-        ajax: "{{ url(config('laraadmin.adminRoute') . '/agenda_dt_ajax') }}",
-		language: {
-			lengthMenu: "_MENU_",
-			search: "_INPUT_",
-			searchPlaceholder: "Procurar"
-		},
-		@if($show_actions)
-		columnDefs: [ { orderable: false, targets: [-1] }],
-		@endif
-	});
-	$("#agenda-add-form").validate({
-		
-	});
-});
-</script>
-@endpush
+	@push('scripts')
+	<script src="{{ asset('la-assets/plugins/datatables/datatables.min.js') }}"></script>
+	<!-- agendamentos -->
+	<script src="{{ asset('la-assets/js/pages/agendas.js') }}"></script>
+	<script>
+		"use strict";
+
+			// function that creates dummy data for demonstration
+			function createDummyData() {
+				var date = new Date();
+				var data = {};
+
+				for (var i = 0; i < 10; i++) {
+					data[date.getFullYear() + i] = {};
+
+					for (var j = 0; j < 12; j++) {
+						data[date.getFullYear() + i][j + 1] = {};
+
+						for (var k = 0; k < Math.ceil(Math.random() * 10); k++) {
+							var l = Math.ceil(Math.random() * 28);
+
+							try {
+								data[date.getFullYear() + i][j + 1][l].push({
+									startTime: "10:00",
+									endTime: "12:00",
+									text: "Some Event Here"
+								});
+							} catch (e) {
+								data[date.getFullYear() + i][j + 1][l] = [];
+								data[date.getFullYear() + i][j + 1][l].push({
+									startTime: "10:00",
+									endTime: "12:00",
+									text: "Some Event Here"
+								});
+							}
+						}
+					}
+				}
+
+				return data;
+			}
+
+			// INSTEAD OF GRABBING THE DATA FROM AN AJAX REQUEST
+			// I WILL BE DEMONSTRATING THE SAME EFFECT THROUGH MEMORY
+			// THIS DEFEATS THE PURPOSE BUT IS SIMPLER TO UNDERSTAND
+			var serverData = createDummyData();
+
+			// stating variables in order for them to be global
+			var calendar, organizer;
+
+			// initializing a new calendar object, that will use an html container to create itself
+			calendar = new Calendar("calendarContainer", // id of html container for calendar
+				"small", // size of calendar, can be small | medium | large
+				[
+					"Domingo", // left most day of calendar labels
+					3 // maximum length of the calendar labels
+					],
+					[
+					"#10cfbd", // primary color
+					"#0eb7a7", // primary dark color
+					"#ffffff", // text color
+					"#3cf0df" // text dark color
+					]
+					);
+
+			// This is gonna be similar to an ajax function that would grab
+			// data from the server; then when the data for a this current month
+			// is grabbed, you just add it to the data object of the form
+			// { year num: { month num: { day num: [ array of events ] } } }
+			function dataWithAjax(date, callback) {
+				var data = {};
+
+				try {
+					data[date.getFullYear()] = {};
+					data[date.getFullYear()][date.getMonth() + 1] = serverData[date.getFullYear()][date.getMonth() + 1];
+				} catch (e) {}
+
+				callback(data);
+			};
+
+			window.onload = function() {
+				dataWithAjax(new Date(), function(data) {
+					// initializing a new organizer object, that will use an html container to create itself
+					organizer = new Organizer("organizerContainer", // id of html container for calendar
+						calendar, // defining the calendar that the organizer is related
+						data // small part of the data of type object
+						);
+
+					// after initializing the organizer, we need to initialize the onMonthChange
+					// there needs to be a callback parameter, this is what updates the organizer
+					organizer.onMonthChange = function(callback) {
+						dataWithAjax(organizer.calendar.date, function(data) {
+							organizer.data = data;
+							callback();
+						});
+					};
+				});
+			};
+		</script>
+		<script>
+			$(function () {
+				$("#example1").DataTable({
+					processing: true,
+					serverSide: true,
+					ajax: "{{ url(config('laraadmin.adminRoute') . '/agenda_dt_ajax') }}",
+					language: {
+						lengthMenu: "_MENU_",
+						search: "_INPUT_",
+						searchPlaceholder: "Procurar"
+					},
+					@if($show_actions)
+					columnDefs: [ { orderable: false, targets: [-1] }],
+					@endif
+				});
+				$("#agenda-add-form").validate({
+
+				});
+			});
+		</script>
+		@endpush
